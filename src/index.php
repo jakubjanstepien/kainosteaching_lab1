a4038e06e50e6dc1cb864f54e5562eb75ed4c2e0
Jakub Stepien <jakub.stepien@kainos.com>
2022-10-28 00:21:44 +0200

    Add files.

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello <br><br>";
+
+
+$host = 'db';
+
+$user = 'MYSQL_USER';
+
+$pass = 'MYSQL_PASSWORD';
+
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+?>
